9b7262d42af919bdd292e0510955648d9e592aad
ryosuke_doi <ryo-62-gnt-0000@outlook.jp>
2022-06-12 21:20:50 +0900

    potepan_javasvcript_task2

---
+++ calculator.js
@@ -0,0 +1,21 @@
+'use strict';
+let display = document.getElementById("display");
+let all_clear = document.getElementById("all_clear");
+let equal = document.getElementById("all_clear");
+
+function btn_click(target){
+  let number = target.innerHTML; 
+  if (number == "=") {
+    display.innerHTML = eval(display.innerHTML) //displayに映っている数字で演算
+    } else if(number == "AC") {
+      display.innerHTML = "0" //AC押した際にdisplayを0に
+    } else {
+      if (display.innerHTML == "0") {
+        display.innerHTML = number //displayが0表示の時、押したbtnを表示
+      } else {
+        display.innerHTML += number //displayの数字+numberをdisplayに加える
+      }
+    }
+}
+
+
